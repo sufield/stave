7c2b28dc14dd5a4002ac66df8ea3dbd9a224e9e4
Sufield <test@example.com>
2026-03-14 10:57:35 -0400

    Remove type alias helpers in prune subcommands, use pruner types directly
        Delete archive/helpers.go, cleanup/helpers.go, and hygiene/snapshot_helpers.go
        which only held type aliases and var aliases marked with TODO comments.
        Callers now reference pruner.SnapshotFile, pruner.Criteria,
        pruneshared.ListObservationSnapshotFiles, and pruneshared.PlanPrune directly.

--- cmd/prune/archive/helpers.go
+++
@@ -1,9 +0,0 @@
-package archive
-
-import (
-	"github.com/sufield/stave/internal/pruner"
-)
-
-type snapshotFile = pruner.SnapshotFile
-
-// TODO : remove this type alias and update all references to use pruner.SnapshotFile instead
